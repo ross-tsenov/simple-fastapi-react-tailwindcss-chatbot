961d6b28f8741750d3617304f3db25520f2f3cbc
Ross Tsenov <ross.tsenov@gmail.com>
2024-11-30 23:31:47 -0500

    Generate Code cleanup

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ChatWindow from './components/ChatWindow';
 
-/**
- * Main application component.
- * @returns {JSX.Element}
- */
 function App() {
   return (
     <div className="flex flex-col h-screen bg-gray-900">
